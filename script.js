8c98c67b58078972a59dc014bc1cd8febace88b7
priyarobinraj <priyarobin2430@gmail.com>
2024-09-22 20:11:28 +0530

    css sep 22-2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const harmburger = document.getElementById("harmburger");
+const harmburger = document.getElementById("hamburger");
 const sidebar = document.getElementById("sidebar");
 const navLinks = document.getElementById("nav");
 
